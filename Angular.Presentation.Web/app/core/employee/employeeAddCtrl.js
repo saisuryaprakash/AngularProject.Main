2346f98337a0240e539d46579afa86bfed664d9c
saisuryaprakash <sai.suryaanbu59@yahoo.com>
2016-03-02 17:57:54 +0530

    surya commit

--- Angular.Presentation.Web/app/core/employee/employeeAddCtrl.js
+++ Angular.Presentation.Web/app/core/employee/employeeAddCtrl.js
@@ -5,24 +5,19 @@ define(['angular-app'], function(app){
     app.controller('employeeAddCtrl', function ($scope) {
 
         debugger;
-        $scope.title = "Employee priya Add Page";
-        $scope.show = "karthi page";
-        $scope.title = "Employee Pfgbo Page";
+        $scope.title = "Employee Add Page";
+       
         
-        $scope.show = "surya page";
+       
         $scope.employeeAdd = [];
-        $scope.testdata = "pfgbo";
-        $scope.newtes = "pfgbo PRIYA";
+       
 
 
-        $scope.test = function () {
-            var a = "test";
-        }
        
         $scope.save = function () {
-            $scope.new = "karthi";
+           
 
-            $scope.test = "merge";
+           
             $scope.employeeAdd.push({
                 empId: $scope.empId,
                 empName: $scope.empName,
